3accaa60b54d4274746adb9dbfe9a27f6aab4c58
Celal Kılınç <celalkilnc@gmail.com>
2022-12-05 18:01:17 +0300

    Update BaseClass.java

--- StrategyGame/src/BaseClass.java
+++ StrategyGame/src/BaseClass.java
@@ -2,8 +2,7 @@ import java.util.Random;
 
 public class BaseClass {
 
-    public FightInfo CharctrFight(Character FstPlyr,Character SndPlyr){
-        //Hiyerarşik karşılaştırma
+    public FightInfo CharctrFight(Character FstPlyr,Character SndPlyr){//Hiyerarşik karşılaştırma
         if(FstPlyr.getId()>SndPlyr.getId())       { return FightInfo.FirstPlayerWin;}
         else if (FstPlyr.getId()<SndPlyr.getId()) { return FightInfo.SecondPlayerWin;}
         else {
@@ -14,21 +13,23 @@ public class BaseClass {
         return FightInfo.Scoreless; //Eşitlik Sağlanırsa
     }
 
-    public Character[] CharacterPack(int CharacterNum){
-        //Karakter destesi döndüren metod
+    public Character[] CharacterPack(int CharacterNum){ //Karakter destesi döndüren metod
         Character[] ListCharacter = new Character[CharacterNum];
+        
         for(int i = 0; i<ListCharacter.length; i++){
             ListCharacter[i] = CreateCharacter();
         }
+        
         return ListCharacter;
     }
 
-    public Character CreateCharacter(){
-        //Karakter oluşturma
+    public Character CreateCharacter(){ //Karakter oluşturma
         Character character = new Character();
         Random random = new Random();
+        
         character.setId(random.nextInt(1,3));
         character.setPower(random.nextInt(1,11));
+        
         return character;
     }
 }
