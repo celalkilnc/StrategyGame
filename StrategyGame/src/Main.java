3966197ee07dbb7106d983e5a2fa34cff2ab6045
Celal Kılınç <celalkilnc@gmail.com>
2022-12-05 22:29:57 +0300

    Update Main.java

--- StrategyGame/src/Main.java
+++ StrategyGame/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args){
         BaseClass baseClass = new BaseClass();
-        int FirstPlyScore = 0,SecondPlyScore = 0,CharacterNum_ = 5;
+        int FirstPlyScore = 0, SecondPlyScore = 0, CharacterNum_ = 5;
 
         Character[] FstPlyrChrtrs = baseClass.CharacterPack(CharacterNum_);
         Character[] SndPlyrChrtrs = baseClass.CharacterPack(CharacterNum_);
