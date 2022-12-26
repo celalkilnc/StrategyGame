9929890060ed54810df9398e7de66cfd03529c46
Celal Kılınç <celalkilnc@gmail.com>
2022-12-26 08:00:52 +0300

    Update Character.java

--- StrategyGame/src/Character.java
+++ StrategyGame/src/Character.java
@@ -1,10 +1,12 @@
 public class Character {
     private int Id; // 1 = Villager 2 = Warrior
     private int power; // 1-10
-
+    
+    // ID operations
     public int  getId()       { return this.Id; }
     public void setId(int id) { this.Id = id;   }
-
+    
+    // Power operations
     public int  getPower()          { return this.power;  }
     public void setPower(int power) { this.power = power; }
 }
